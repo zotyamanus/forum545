2c3335d5abec45aea02be0d07609584ac80631ed
zoli <asd>
2025-09-19 11:31:05 +0200

    index php alap html tartalommal

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>forum</title>
+</head>
+<body>
+    <h1>Témák:</h1>
+</body>
+</html>
