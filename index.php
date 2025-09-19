3df9724b56bf0badf32a9be41b0464dda3aa5227
zoli <asd>
2025-09-19 11:41:56 +0200

    POST TESZT HOZZÁADVA

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+$szoveg ="";
+if(isset($_POST["topic"])){
+    $szoveg = "kaptam egy új topic post adatot";
+}
+?>
+
 <!DOCTYPE html>
 <html lang="hu">
 <head>
@@ -6,6 +13,10 @@
     <title>forum</title>
 </head>
 <body>
+    
+<?php
+echo $szoveg
+?>
     <h1>Témák:</h1>
     <form method=POST>
         <input type="text" name="topic">
