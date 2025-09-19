f05d3c98497a05dec74066969087f7ac6914f44f
zoli <asd>
2025-09-19 11:55:41 +0200

    json data adaton elöre beolvasva

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <?php
+$filename = "data.json";
+if(file_exists($filename)){
+    $jsonString = file_get_contents($filename);
+    $topics = json_decode($jsonString);
+}else{
+    $topics=[];
+}
+
+
 $szoveg ="";
 if(isset($_POST["topic"])){
     $topics=[];
     array_push($topics, $_POST["topic"]);
     $jsonString= json_encode($topics);
     $szoveg = $jsonString;
-    $filename = "data.json";
     file_put_contents($filename, $jsonString);
 }
 ?>
